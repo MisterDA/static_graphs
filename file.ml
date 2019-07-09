1b602b5a47dc4d3cbe33fcf0b4a964db931f5fe7
Antonin Décimo <antonin.decimo@gmail.com>
2019-07-09 15:25:39 +0200

    Read whole Gzip file at once

--- file.ml
+++ file.ml
@@ -1,70 +1,66 @@
-module Bytes' = Bytes
-module Bytes = struct
-  include Bytes'
-  let index_from_upto buf start len c =
-    let idx = ref (-1) in
-    try
-      for i = start to len - 1 do
-        if Bytes.get buf i = c then (idx := i; raise Exit)
-      done;
-      None
-    with Exit -> Some (!idx)
+(* module Bytes = struct
+ *   include Bytes
+ *
+ *   (\* let index_from_upto buf start len c =
+ *    *   let idx = ref (-1) in
+ *    *   try
+ *    *     for i = start to len - 1 do
+ *    *       if Bytes.get buf i = c then (idx := i; raise Exit)
+ *    *     done;
+ *    *     None
+ *    *   with Exit -> Some (!idx) *\)
+ *
+ *   (\* let print buf = Seq.iter (fun c -> print_char c) (Bytes.to_seq buf) *\)
+ *   (\* let print_upto buf len =
+ *    *   for i = 0 to min (Bytes.length buf) len - 1 do
+ *    *     print_char (Bytes.get buf i)
+ *    *   done *\)
+ * end *)
 
-  (* let print buf = Seq.iter (fun c -> print_char c) (Bytes.to_seq buf) *)
-  let print_upto buf len =
-    for i = 0 to min (Bytes.length buf) len - 1 do
-      print_char (Bytes.get buf i)
-    done
-end
+let cap = 200_000_000 (* 200 megabytes *)
 
 type format = Csv | Tuples
-type buf = { mutable buf : bytes; mutable len : int; mutable cap : int}
 type t =
   | File of Stdlib.in_channel * format
-  | Gzip of Gzip.in_channel * buf * format
+  | Gzip of Gzip.in_channel * (bytes * int ref) * format
 
 let split fmt line =
   String.split_on_char (match fmt with Csv -> ',' | Tuples -> ' ') line
 
-let input_line parse = function
+let input_line parse f =
+  match f with
   | File (chan, fmt) ->
      let line = input_line chan in
      let last = String.length line - 1 in
      (if String.get line last = '\r' then String.sub line 0 last else line)
      |> split fmt |> parse
-  | Gzip (chan, b, fmt) ->
-     let str = ref None in
-     while !str = None do
-       match Bytes.index_from_upto b.buf 0 b.len '\n' with
-       | None ->
-          b.len <- b.len + Gzip.input chan b.buf b.len (b.cap - b.len);
-          if b.len = 0 then raise End_of_file;
-       | Some i when i > 0 && Bytes.get b.buf (i-1) = '\r' ->
-          str := Some (Bytes.sub_string b.buf 0 (i-1));
-          b.len <- b.len - i;
-          Bytes.print_upto b.buf b.len;
-          if b.len > 0 then Bytes.blit b.buf (i+1) b.buf 0 b.len
-       | Some i ->
-          str := Some (Bytes.sub_string b.buf 0 i);
-          b.len <- b.len - (i+1);
-          if b.len > 0 then Bytes.blit b.buf (i+1) b.buf 0 b.len
-     done;
-     match !str with
-     | None -> assert false
-     | Some str -> let () = split fmt str |> parse in ()
+  | Gzip (_, (s, cur), fmt) ->
+     let i = Bytes.index_from s !cur '\n' in
+     let j = if Bytes.get s (i - 1) = '\r' then i - 1 else i in
+     let str = Bytes.sub_string s !cur (j - !cur) in
+     print_endline str;
+     cur := i + 1;
+     split fmt str |> parse
 
 let open_in path fmt =
   let n = String.length path in
-  let f = begin
-      if String.sub path (n - 3) 3 = ".gz" then
-        try Gzip (Gzip.open_in path,
-                  {buf = Bytes.create 256; len = 0; cap = 256}, fmt)
-        with _ -> File (open_in (String.sub path 0 (n - 3)), fmt)
-      else
-        try Gzip (Gzip.open_in (path ^ ".gz"),
-                  {buf = Bytes.create 256; len = 0; cap = 256}, fmt)
-        with _ -> File (open_in path, fmt)
-    end in
+  let f =
+    if String.sub path (n - 3) 3 = ".gz" then
+      try Gzip (Gzip.open_in path, (Bytes.create cap, ref 0), fmt)
+      with _ -> File (open_in (String.sub path 0 (n - 3)), fmt)
+    else
+      try
+        let path = path ^ ".gz" in
+        Gzip (Gzip.open_in path, (Bytes.create cap, ref 0), fmt)
+      with _ -> File (open_in path, fmt)
+  in
+  begin match f with
+    | Gzip (chan, (s, _), _) ->
+       begin try Gzip.really_input chan s 0 cap;
+                 failwith "Not enough buffer allocated."
+             with End_of_file -> () end;
+    | File _ -> ()
+  end;
   match fmt with
   | Tuples -> f
   | Csv -> input_line (fun _ -> ()) f; f
